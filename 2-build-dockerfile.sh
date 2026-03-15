6e8b4fc35287b7a37fcccf95b63dee806e683f4a
David <david@kronlid.net>
2026-03-15 14:04:10 +0100

    Added a few extra tools

--- 2-build-dockerfile.sh
+++ 2-build-dockerfile.sh
@@ -4,8 +4,10 @@ set -euo pipefail
 # --- Konfiguration ---
 IMAGE_NAME="davidkronlid/python-rust"
 
+VERSION_TAG=python3.13_rust1.94
+
 # --- Datumtagg ---
-DATE_TAG=$(date +%Y-%m-%d)
+DATE_TAG=${VERSION_TAG}_$(date +%Y-%m-%d)
 
 echo "Bygger Docker-image..."
-docker build -t "${IMAGE_NAME}:${DATE_TAG}" -t "${IMAGE_NAME}:latest" .
+docker build -t "${IMAGE_NAME}:${VERSION_TAG}" -t "${IMAGE_NAME}:${DATE_TAG}" -t "${IMAGE_NAME}:latest" .
